1e302d95afaab10e1677393e775c61788635dd0c
samuelwong30 <75662801+samuelwong30@users.noreply.github.com>
2021-02-20 16:46:23 -0800

    josh's class

--- goat.py
+++ goat.py
@@ -1,10 +1,34 @@
-# Here's where we put the class
 class Person():
 
-	# Constructor
-	def __init__(self):
+    # Constructor
+    def init(self):
+        pass
 
 
-	# Methods
-	def methodname(self):
-		
+    # Methods
+    def setter(self, name, instagram, twitter, phone):
+        f = open("friends.txt","a",)
+        f.write("{"+ name + ","
+                   + instagram + ","
+                   + twitter + ","
+                   + phone + "}},"
+                    )
+        f.close()
+
+    def del(self):
+        pass
+
+
+    def getter(self,name):
+        with open("friends.txt","r") as file:
+            data = file.read()
+        lines = data.split('},')
+        for line in lines:
+            v = line.split(',')
+            if name == v[0][1:]:
+                v[0] == v[0][1:]
+                line =  line.split(',')
+                line[0] = line[0][1:]
+                line[3] = line[3][:-1]
+                return line
+        return ["-1"]
